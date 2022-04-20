f2cfeb2ffe7a1f09124f75e40f0f7e3ee79e72c5
netovieira01_ <netinhovieira1801@gmail.com>
2022-04-20 09:24:24 -0300

    A imutabilidade no React utilizando o useState

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,26 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet, TextInput, Platform, TouchableOpacity } from 'react-native'
 
 export function Home() {
+    const [newSkill, setNewSkill] = useState('');
+    const [mySkills, setMySkills] = useState([]);
+
+    function handleAddNewSkill() {
+        setMySkills(oldState => [...oldState, newSkill]);
+    }
+
     return (
         <View style={styles.container}>
+
             <Text
                 style={styles.title}>Welcome, Manoel
             </Text>
+
             <TextInput
                 style={styles.input}
                 placeholder='New Skill'
                 placeholderTextColor='#444'
+                onChangeText={setNewSkill}
             />
-            <TouchableOpacity 
-            style={styles.button}
-            activeOpacity={.7}
+
+            <TouchableOpacity
+                style={styles.button}
+                activeOpacity={.7}
+                onPress={handleAddNewSkill}
             >
-                <Text style={styles.buttonText}>Add</Text>
+                <Text
+                    style={styles.buttonText}>Add
+                </Text>
             </TouchableOpacity>
-            <Text style={[styles.title, {marginTop: 50}]}>
+
+            <Text style={[styles.title, { marginVertical: 35 }]}>
                 My Skills
             </Text>
+
+            {
+                mySkills.map(skill => (
+                    <TouchableOpacity key={skill} style={styles.buttonSkill}>
+                        <Text style={styles.textSkill}>
+                            {skill}
+                        </Text>
+                    </TouchableOpacity>
+                ))
+            }
+
         </View>
     )
 }
@@ -58,5 +84,18 @@ const styles = StyleSheet.create({
         color: 'white',
         fontSize: 16,
         fontWeight: 'bold'
+    },
+    buttonSkill: {
+        backgroundColor: '#1f1e25',
+        padding: 15,
+        borderRadius: 40,
+        alignItems: 'center',
+        marginVertical: 10
+    },
+    textSkill: {
+        color: 'white',
+        fontSize: 18,
+        fontWeight: 'bold',
+
     }
 })
