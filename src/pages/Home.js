2555f0f7e368b1a86f8a0b907f5772f282b97c05
netovieira01_ <netinhovieira1801@gmail.com>
2022-04-20 08:31:43 -0300

    Utilizando StyleSheet

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,62 @@
 import React from "react";
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, TextInput, Platform, TouchableOpacity } from 'react-native'
 
 export function Home() {
     return (
-        <>
-            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <Text>React Native</Text>
-                <Text>Ignite</Text>
-            </View>
-        </>
+        <View style={styles.container}>
+            <Text
+                style={styles.title}>Welcome, Manoel
+            </Text>
+            <TextInput
+                style={styles.input}
+                placeholder='New Skill'
+                placeholderTextColor='#444'
+            />
+            <TouchableOpacity 
+            style={styles.button}
+            activeOpacity={.7}
+            >
+                <Text style={styles.buttonText}>Add</Text>
+            </TouchableOpacity>
+            <Text style={[styles.title, {marginTop: 50}]}>
+                My Skills
+            </Text>
+        </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#121015',
+        paddingHorizontal: 20,
+        paddingVertical: 70,
+        paddingHorizontal: 30,
+    },
+    title: {
+        color: '#fff',
+        fontSize: 24,
+        fontWeight: 'bold'
+    },
+    input: {
+        backgroundColor: '#1f1e25',
+        color: 'white',
+        fontSize: 16,
+        padding: Platform.OS === 'ios' ? 15 : 10,
+        marginTop: 25,
+        borderRadius: 8
+    },
+    button: {
+        backgroundColor: '#A370F7',
+        padding: Platform.OS === 'ios' ? 15 : 12,
+        borderRadius: 8,
+        display: 'flex',
+        alignItems: 'center',
+        marginTop: 20,
+    },
+    buttonText: {
+        color: 'white',
+        fontSize: 16,
+        fontWeight: 'bold'
+    }
+})
